f196edc9b7732a846ef8c1afe27cef856069d42a
@Donskoy <diman@arocketmail.com>
2018-04-02 12:56:58 -0500

    some logic working

--- assets/javascript/trainLogic.js
+++ assets/javascript/trainLogic.js
@@ -1,101 +1,4 @@
-console.log("train logic javascript")
 
-/* global firebase */
-
-// Initialize Firebase
-// Make sure that your configuration matches your firebase script version
-// (Ex. 3.0 != 3.7.1)
-var config = {
-    apiKey: "AIzaSyB48Th2pp0_4KFw_G7uJHBeD_ImoOStKtA",
-    authDomain: "dimas-project-e058f.firebaseapp.com",
-    databaseURL: "https://dimas-project-e058f.firebaseio.com",
-    projectId: "dimas-project-e058f",
-    storageBucket: "dimas-project-e058f.appspot.com",
-    messagingSenderId: "302948182156"
-  };
-  
-  firebase.initializeApp(config);
-  
-  // Create a variable to reference the database
-  var database = firebase.database();
-  
-  // Use the below initialValue
-  var initialValue = 100;
-  
-  // Use the below variable clickCounter to keep track of the clicks.
-  var clickCounter = initialValue;
-  
-  // --------------------------------------------------------------
-  
-  // At the initial load and on subsequent data value changes, get a snapshot of the current data. (I.E FIREBASE HERE)
-  // This callback keeps the page updated when a value changes in firebase.
-  database.ref().on("value", function(snapshot) {
-    // We are now inside our .on function...
-  
-    // Console.log the "snapshot" value (a point-in-time representation of the database)
-    console.log(snapshot.val());
-    // This "snapshot" allows the page to get the most current values in firebase.
-  
-    // Change the value of our clickCounter to match the value in the database
-    clickCounter = snapshot.val().clickCount;
-  
-    // Console Log the value of the clickCounter
-    console.log(clickCounter);
-  
-    // Change the HTML using jQuery to reflect the updated clickCounter value
-    $("#click-value").text(snapshot.val().clickCount);
-    // Alternate solution to the above line
-    //$("#click-value").html(clickCounter);
-  
-  // If any errors are experienced, log them to console.
-  }, function(errorObject) {
-    console.log("The read failed: " + errorObject.code);
-  });
-  
-  // --------------------------------------------------------------
-  
-  // Whenever a user clicks the click button
-  $("#click-button").on("click", function() {
-  
-    // Reduce the clickCounter by 1
-    clickCounter--;
-  
-    // Alert User and reset the counter
-    if (clickCounter === 0) {
-      alert("Phew! You made it! That sure was a lot of clicking.");
-      clickCounter = initialValue;
-    }
-  
-    // Save new value to Firebase
-    database.ref().set({
-      clickCount: clickCounter
-    });
-  
-    // Log the value of clickCounter
-    console.log(clickCounter);
-  
-  });
-  
-  // Whenever a user clicks the restart button
-  $("#restart-button").on("click", function() {
-  
-    // Set the clickCounter back to initialValue
-    clickCounter = initialValue;
-  
-    // Save new value to Firebase
-    database.ref().set({
-      clickCount: clickCounter
-    });
-  
-    // Log the value of clickCounter
-    console.log(clickCounter);
-  
-    // Change the HTML Values
-    $("#click-value").text(clickCounter);
-  
-  });
-
-  
   //-------------------------------------------------------------------------------------------------------------------------
 
 
@@ -103,8 +6,8 @@ var config = {
 // Steps to complete:
 
 // 1. Initialize Firebase
-// 2. Create button for adding new employees - then update the html + update the database
-// 3. Create a way to retrieve employees from the employee database.
+// 2. Create button for adding new trains - then update the html + update the database
+// 3. Create a way to retrieve trains from the train database.
 // 4. Create a way to calculate the months worked. Using difference between start and current time.
 //    Then use moment.js formatting to set difference in months.
 // 5. Calculate Total billed
@@ -178,11 +81,11 @@ var config = {
     console.log(trainRate);
   
     // Prettify the train start
-    var trainStartPretty = moment.unix(empStart).format("MM/DD/YY");
+    var trainStartPretty = moment.unix(trainStart).format("MM/DD/YY");
   
     // Calculate the months scheduled using hardcore math
     // To calculate the months arrivals
-    var trainMonths = moment().diff(moment.unix(empStart, "X"), "months");
+    var trainMonths = moment().diff(moment.unix(trainStart, "X"), "months");
     console.log(trainMonths);
   
     // Calculate the total billed rate
@@ -190,7 +93,7 @@ var config = {
     console.log(trainScheduled);
   
     // Add each train's data into the table
-    $("#employee-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" +
+    $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" +
     trainStartPretty + "</td><td>" + trainMonths + "</td><td>" + trainRate + "</td><td>" + trainScheduled + "</td></tr>");
   });
   
